6fcd688039b7f789f7f23c6ed110109fcbc10e89
clayton <clayton.a.butler@gmail.com>
2020-09-26 16:02:53 -0400

    add NotFound component

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,10 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About,
+  },
+  {
+    path: '*',
+    component: NotFound,
   },
 ];
 
