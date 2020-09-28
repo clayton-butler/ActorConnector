d70be1f2ce38db85421d45a216ca3b5f51d0c322
clayton <clayton.a.butler@gmail.com>
2020-09-28 13:36:33 -0400

    misc

---
+++ client/src/components/Instructions.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>
+    Begin typing in the fields below to search for actors to connect,
+    or press <b-icon-shuffle></b-icon-shuffle> to select an actor at random.
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'Instructions',
+};
+</script>
+
+<style scoped>
+
+</style>
--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="notfound">
+  <div class="notfound fill-height">
     <ac-header></ac-header>
     <b-container fluid>
       <b-row class="justify-content-center">
-        <b-col></b-col>
         <b-col>
-          <div style="bottom: 0; position: fixed">
-            <h1>404</h1>
-            <h2>Page Not Found</h2>
+          <h1>404</h1>
+          <h2>Page Not Found</h2>
+          <div class="fixed-bottom">
             <img src="img/confused_travolta.jpg" alt=""/>
           </div>
         </b-col>
-        <b-col></b-col>
       </b-row>
     </b-container>
   </div>
@@ -30,5 +28,8 @@ export default {
 </script>
 
 <style scoped>
-
+  .fill-height {
+    min-height: 95%;
+    height: 95%;
+  }
 </style>
