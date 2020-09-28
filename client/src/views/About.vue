2898f49ace07e08718e0def49e6fbe10e6d5e0f0
clayton <clayton.a.butler@gmail.com>
2020-09-28 13:08:44 -0400

    updated about page

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -4,15 +4,26 @@
     <b-container fluid="">
       <b-row>
         <b-col></b-col>
-        <b-col md="8">
-          <h3 class="section-header">About</h3>
-          <hr>
-          {{actor_count.toLocaleString()}} actors, playing {{role_count.toLocaleString()}}
-          characters in {{movie_count.toLocaleString()}} movies and
-          {{episode_count.toLocaleString()}} episodes of {{series_count.toLocaleString()}} TV shows.
+        <b-col md="5">
+          <div class="about-section">
+            <h3 class="section-header">About</h3>
+            <hr>
+            <p class="text-left">
+              Everyone has played the party game "Six Degrees of Kevin Bacon", but what about
+              "Eight Degrees of Achim Schmidtchen"? Or "Four Degrees of Yoneko Sakai"? Or "Five
+              Degrees of Brian Hatch"? The Actor Connector can connect almost any two film actors
+              together, from anywhere in the world, and using films going back to the start of the
+              20th century. This site is an exercise in graph theory, and utilizes a graph
+              consisting of {{actor_count.toLocaleString()}} actors, playing
+              {{role_count.toLocaleString()}} characters in {{movie_count.toLocaleString()}}
+              movies and {{episode_count.toLocaleString()}} episodes of
+              {{series_count.toLocaleString()}} TV shows.
+            </p>
+          </div>
+          <div class="about-section">
           <h3 class="section-header">Credits</h3>
           <hr>
-          Actor, movie, and television show information courtesy of
+          Actor, movie, and television show information <br>courtesy of
           <div>
             <a href="https://www.imdb.com" target="_blank" rel="noopener noreferrer">
               <img src="img/IMDB_logo.png" alt="IMDB"/>
@@ -21,16 +32,25 @@
           Used with permission.
           <br>
           <br>
+
           Profile and poster images courtesy of
           <div>
             <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">
               <img src="img/TMDB_logo.png" alt="TMDB">
             </a>
           </div>
-          This product uses the TMDb API but is not endorsed or certified by TMDb.
+          This product uses the TMDb API but is <br>not endorsed or certified by TMDb.
+          </div>
           <h3 class="section-header">Source</h3>
           <hr>
-          github link or something
+          The source for this site, as well as the source for <br>building the graph database
+          can be found
+          <a href="https://github.com/clayton-butler/ActorConnector" target="_blank" rel="noopener roreferrer">
+            here
+          </a>.
+          <br>
+          <br>
+          <br>
         </b-col>
         <b-col></b-col>
       </b-row>
@@ -78,5 +98,7 @@ export default {
 <style scoped>
   .section-header {
     margin-top: 2em;
+  }
+  about-section {
   }
 </style>
